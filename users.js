4c3b9373c0a6d8950358155b1cfb3fded8e5dfb3
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2025-04-20 14:41:43 -0400

    exposed user data

---
+++ users.js
@@ -0,0 +1,12 @@
+const users = [
+  {
+    username: 'Sophia',
+    password: 'sophia',
+    visitorId: ['I0RcG2JkAyiwBzzAdDu6'],
+  },
+  { username: 'Trey', password: 'trey', visitorId: [] },
+  { username: 'Tulo', password: 'tulo', visitorId: [] },
+  { username: 'User', password: 'password', visitorId: [] },
+];
+
+export default users;

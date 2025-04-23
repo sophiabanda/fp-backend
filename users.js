1065cb0db26930796bf9d13c0452e37a8e46b07d
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2025-04-23 16:02:38 -0400

    added another vId to uesrs

--- users.js
+++ users.js
@@ -2,7 +2,7 @@ const users = [
   {
     username: 'Sophia',
     password: 'sophia',
-    visitorId: ['I0RcG2JkAyiwBzzAdDu6'],
+    visitorId: ['I0RcG2JkAyiwBzzAdDu6', '0yvyTlHhC3u5PFNEGlRw'],
   },
   { username: 'Trey', password: 'trey', visitorId: [] },
   { username: 'Tulo', password: 'tulo', visitorId: [] },
